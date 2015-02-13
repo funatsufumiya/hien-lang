65b9472e228600ce9e49c37a8e2be6d6ed0b65b6
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2015-02-13 18:20:48 +0900

    Updated parser

--- src/ast.ml
+++ src/ast.ml
@@ -1,7 +1,15 @@
 #module Ast =
 # struct
 #   type ast =
-#     REQUIRE of string
+#     | UNDEFINED
+#     | REQUIRE of string
+#     | LET of declareMap list
+#     | VAR of declareMap list
+#     | STRING of string
+#     | INT of int
+#     | FLOAT of float
+#     | FUNCTION of string * string list * ast list
+#   type declareMap = {name: string; expr: ast}
 # end;;
 
 
